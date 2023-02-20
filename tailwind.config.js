a02d407f5123d905b50f08fd63ce7c8098e5f50f
Sduttt <subhamdutta588@gmail.com>
2023-02-20 07:47:34 +0530

    First commit, Completed design for laptop screen

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    fontWeight:{
+      'semibold': '600',
+      'bold' : '700',
+      'boldest' : '900'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
